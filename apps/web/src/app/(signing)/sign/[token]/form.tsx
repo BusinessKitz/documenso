9d17956353dd4b64516a44879601d5c0d3d7967a
denis-zhydal <den.only88@gmail.com>
2024-07-30 00:27:22 +0400

    fix: lint updates

--- apps/web/src/app/(signing)/sign/[token]/form.tsx
+++ apps/web/src/app/(signing)/sign/[token]/form.tsx
@@ -102,7 +102,7 @@ export const SigningForm = ({ document, recipient, fields, redirectUrl }: Signin
       onSubmit={handleSubmit(onFormSubmit)}
     >
       {validateUninsertedFields && uninsertedFields[0] && (
-        <FieldToolTip key={uninsertedFields[0].id} field={uninsertedFields[0]}  color="warning">
+        <FieldToolTip key={uninsertedFields[0].id} field={uninsertedFields[0]} color="warning">
           Click to confirm
         </FieldToolTip>
       )}
